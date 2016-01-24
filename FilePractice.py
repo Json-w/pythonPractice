bc94fa11c14e169ca430da13857080ebc1ae7eb6
wang pei <519875872@qq.com>
2016-01-24 22:34:59 +0800

    foget

--- FilePractice.py
+++ FilePractice.py
@@ -1,8 +1,12 @@
+import os
 with open('/home/jason/python/helloFlask.py', 'r') as f:
-    for line in f:
-        print(line)
+    print(f.name)
+    print(os.path.dirname(f.name))
+    print(os.path.exists(f.name))
+    # for line in f:
+    #     print(line)
 
-with open('/home/jason/python/filePractice.py', 'w') as wf:
-    wf.write('print(hello world)')
+# with open('/home/jason/python/filePractice.py', 'w') as wf:
+#     wf.write('print(hello world)')
 
 
